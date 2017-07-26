a4c02865f58cd622fa316c45aff1a7f466488bc8
Tyson Gern <tgern@pivotal.io>
2017-07-26 12:28:32 -0600

    actuator commit

--- src/test/java/test/pivotal/pal/tracker/TimeEntryControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/TimeEntryControllerTest.java
@@ -5,6 +5,8 @@ import io.pivotal.pal.tracker.TimeEntryController;
 import io.pivotal.pal.tracker.TimeEntry;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -20,12 +22,17 @@ import static org.mockito.Mockito.verify;
 
 public class TimeEntryControllerTest {
     private TimeEntryRepository timeEntryRepository;
+    private CounterService counter;
+    private GaugeService gauge;
     private TimeEntryController controller;
+
 
     @Before
     public void setUp() throws Exception {
         timeEntryRepository = mock(TimeEntryRepository.class);
-        controller = new TimeEntryController(timeEntryRepository);
+        counter = mock(CounterService.class);
+        gauge = mock(GaugeService.class);
+        controller = new TimeEntryController(timeEntryRepository,counter,gauge);
     }
 
     @Test
