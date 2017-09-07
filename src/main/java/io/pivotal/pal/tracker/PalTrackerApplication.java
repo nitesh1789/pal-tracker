1ec6e4ecd60e90c59871fcae400caeb5c176315a
RAWAT <nitesh.rawat@capgemini.com>
2017-09-07 08:13:45 +0530

    jdbc update

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -3,6 +3,9 @@ package io.pivotal.pal.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -10,8 +13,9 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
         @Bean
-        TimeEntryRepository timeEntryRepository() {
-            return new InMemoryTimeEntryRepository();
+        JdbcTimeEntryRepository jdbcTimeEntryRepository(DataSource dataSource) {
+            return new JdbcTimeEntryRepository(dataSource);
         }
+
     }
 
